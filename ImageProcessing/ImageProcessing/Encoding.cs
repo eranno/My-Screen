a1ba4683b5c120dfa68b1de6ca09a6c029f67870
ilandbt <ilandbt@gmail.com>
2012-11-27 23:39:10 +0200

    adding image processing classes

---
+++ ImageProcessing/ImageProcessing/Encoding.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace ImageProcessing
+{
+
+    class Encoding
+    {
+
+
+        Image inputImage;
+        Bitmap inputBitmap;
+        int SWAP = 300;
+        int[,] path;
+        String imageIndex;
+        // Bitmap b2;
+
+        public Encoding(String url, string index)
+        {
+            imageIndex = index;
+            using (WebClient wc = new WebClient())
+            {
+                Stream strm = null;
+
+                try
+                {
+                    strm = wc.OpenRead(url);
+                    inputImage = Image.FromStream(strm);
+                    inputBitmap = new Bitmap(inputImage);
+                }
+                finally
+                {
+                    if (strm != null)
+                        strm.Close();
+                }
+
+
+                inputBitmap.Save("C:\\Users\\user\\pics\\stage\\fromWeb" + imageIndex + ".jpg");
+
+            }
+        }
+
+
+
+
+        public static System.Drawing.Bitmap Resize(System.Drawing.Bitmap value, int newWidth, int newHeight)
+        {
+            System.Drawing.Bitmap resizedImage = new System.Drawing.Bitmap(newWidth, newHeight);
+            System.Drawing.Graphics.FromImage((System.Drawing.Image)resizedImage).DrawImage(value, 0, 0, newWidth, newHeight);
+            return (resizedImage);
+        }
+
+    }
+}
---
+++ ImageProcessing/ImageProcessing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding e = new Encoding("http://vulcan.wr.usgs.gov/Imgs/Jpg/MSH/Images/MSH64_aerial_view_st_helens_from_NE_09-64_med.jpg", "0");
+        }
+    }
+}
