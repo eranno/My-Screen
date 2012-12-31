917f5af2cb44bc79698271ad6e610e73eda512f4
Eran <erannn@gmail.com>
2013-01-01 01:28:15 +0200

    server

--- server/act/server.php
+++ server/act/server.php
@@ -1,20 +1,30 @@
 <?php
 $json = '{	
+			//login details. must be on every connnection
 			"login":
 			{
 				"id":"1",
 				"password":"1234"
 			},
+			
+			
+			//### optional values ###//
+			
+			//add by emails - only members will be signed
 			"add_contacts":
 			{
 				"ilan@gmail.com",
 				"krantz@gmail.com"
 			},
+			
+			//add images - encrypted images name
 			"add_images":
 			{
 				"duygkiu76576445twdtg",
 				"trdcfujhkhgfuy3465457"
 			},
+			
+			//add permission to friends by image for each user
 			"add_permissions":
 			{
 				"user":"ilan@gmail.com",
@@ -31,7 +41,44 @@ $json = '{
 					"duygkiu76576445twdtg",
 					"trdcfujhkhgfuy3465457"
 				}
+			},
+
+
+			//## removes are the same as adds
+
+			//remove by emails - only members will be signed
+			"remove_contacts":
+			{
+				"ilan@gmail.com",
+				"krantz@gmail.com"
+			},
+			
+			//remove images - encrypted images name
+			"remove_images":
+			{
+				"duygkiu76576445twdtg",
+				"trdcfujhkhgfuy3465457"
+			},
+			
+			//remove permission to friends by image for each user
+			"remove_permissions":
+			{
+				"user":"ilan@gmail.com",
+				"images":
+				{
+					"duygkiu76576445twdtg",
+					"trdcfujhkhgfuy3465457"
+				}
+			},
+			"remove_permissions":{
+				"user":"krantz@gmail.com",
+				"images":
+				{
+					"duygkiu76576445twdtg",
+					"trdcfujhkhgfuy3465457"
+				}
 			}
+
 		}';
 			
 $s = new server($json);
@@ -45,6 +92,7 @@ class server
 
 	//user inputs
 	private $json;	//json input
+	private	$id;
 	private	$email;
 	private $pass;
 
@@ -54,14 +102,13 @@ class server
 
 	//possible actions
 	private $act = array(
-		"login" 			=> 0,
-		"add_contacts" 		=> 0,
-		"add_images" 		=> 0,
-		"add_permissions" 	=> 0,
-		"change_password" 	=> 0,
-		"remove_contact" 	=> 0,
-		"remove_image" 		=> 0,
-		"remove_permission" => 0
+		"login" 				=> 0,	//zero is counter, for future use.
+		"add_contacts" 			=> 0,
+		"add_images" 			=> 0,
+		"add_permissions" 		=> 0,
+		"remove_contacts" 		=> 0,
+		"remove_images" 		=> 0,
+		"remove_permissions" 	=> 0
 	);
 
     public function __construct($arr)
@@ -93,6 +140,7 @@ class server
 
 	public function user($arr)
     {
+		$this->id 		= $arr["id"];
 		$this->email 	= $arr["email"];
 		$this->pass 	= $arr["password"];
 		return 0;
