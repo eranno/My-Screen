ab4dafba321f5c095b15e5f5560a6c0de4ec8d9a
Noam Tzumie <noam185@gmail.com>
2013-01-07 19:23:20 +0200

    adding login

---
+++ Client/loginHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DataHandler;
+
+namespace loginHandler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DBHandler.initDB();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
