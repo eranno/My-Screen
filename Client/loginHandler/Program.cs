49cfa8afa7df73e5f757eb0fcad842c4bf61922d
Noam Tzumie <noam185@gmail.com>
2013-01-07 16:15:18 +0200

    dsd

---
+++ Client/loginHandler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DataHandler;
+
+namespace loginHandler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int i = 0;
+            DBHandler.initDB();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
