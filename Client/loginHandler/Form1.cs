55b1d41a1bccfdaeb0ab89b0712654a907a09740
davidkrantzy <davidkrantzy@gmail.com>
2013-01-21 18:43:58 +0200

    Update Client/loginHandler/Form1.cs
    taken off the button handler

--- Client/loginHandler/Form1.cs
+++ Client/loginHandler/Form1.cs
@@ -22,39 +22,7 @@ namespace loginHandler
             InitializeComponent();
         }
 
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show("Submit!");
-            error.Visible = false;
-            if (password.Text != reEnterPassword.Text)
-            {
-                password.Text = null;
-                reEnterPassword.Text = null;
-                error.Text = PASSWORD_DOESNT_MACH;
-                error.Visible = true;
-                return;
-            }
-            if (name.Text == "" || email.Text == "" ||
-                password.Text == "" || reEnterPassword.Text == "")
-            {
-                error.Text = PARAM_MISSING;
-                error.Visible = true;
-                return;
-            }
-
-            if (!validEmail(email.Text, password.Text))
-            {
-                error.Text = EMAIL_NOT_VALID;
-                error.Visible = true;
-                return;
-            }
-            //set new values in user's properties table.
-            Boolean success = setProperties(name.Text, email.Text, password.Text);
-            Form2 form2 = new Form2();
-            this.Close();
-            form2.Show();
-        }
-
+        
         //checks if email is valid
         private Boolean validEmail(String email, String password)
         {
